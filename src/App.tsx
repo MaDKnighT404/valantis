989c3b7dde816ddd562a5f3f9ccaa422a9be7938
George Koloidi <Battleaxe@bk.ru>
2024-02-28 22:04:40 +0100

    refactor: change some styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { FilterForm } from './components/FilterForm';
 import { ItemsList } from './components/ItemsList';
 
 const App = () => {
   return (
-    <main className="h-screen">
-      <div className="container mx-auto flex  flex-col items-center justify-center">
+    <main>
+      <div className="mx-auto flex max-w-[1200px] flex-col items-center justify-center">
         <h1 className="text-center text-4xl text-teal-700">
           Test Task Valantis
         </h1>
+        <FilterForm />
         <ItemsList />
       </div>
     </main>
