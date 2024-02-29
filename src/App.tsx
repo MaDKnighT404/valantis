f4f3f504acb10fcc98efe755d1b3c15b9f39f660
George Koloidi <Battleaxe@bk.ru>
2024-02-29 19:16:52 +0100

    feat: add Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { PageSelector } from './components/PageSelector';
 import { ProductsFilterForm } from './components/ProductsFilterForm';
 import { ProductsList } from './components/ProductsList';
 import { useProductStore } from './store';
+import { Footer } from './components/Footer';
 
 const App = () => {
   const { isFilterActive } = useProductStore((state) => ({
@@ -11,13 +12,14 @@ const App = () => {
   return (
     <main>
       <div className="mx-auto flex max-w-[1200px] flex-col items-center justify-center">
-        <h1 className="text-center text-4xl text-teal-700">
+        <h1 className="my-6 text-center text-4xl text-teal-700">
           Test Task Valantis
         </h1>
         <ProductsFilterForm />
         <PageSelector />
         {isFilterActive ? <FilteredProductsList /> : <ProductsList />}
       </div>
+      <Footer />
     </main>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-black text-white">
+      <h4 className="flex flex-wrap justify-center whitespace-pre p-4">
+        <a href="https://github.com/MaDKnighT404">Georgii Koloidi || </a>
+        <span>Battleaxe@bk.ru || </span> <span>+7 918 204 28 88</span>
+      </h4>
+    </footer>
+  );
+};
+
+
